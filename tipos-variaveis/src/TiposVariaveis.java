d221e1639bdee18885c4cfdce81302a2a1c7a525
Jose Carlos Viana <vianalisboa316@gmail.com>
2025-04-10 18:51:48 -0300

    Tipos de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,22 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        // Iniciando...
+        // Tipos primitivos
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 12345-678;
+        long cpf = 12345678910L;
+        float pi = 3.14F;
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal; // Casting
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        // Constantes
+        final double VALOR_DE_PI  = 3.14;
     }
 }
