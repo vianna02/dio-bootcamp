c84f2cf39907a402bb0a4c5c613d2120fb45d628
vianna02 <vianalisboa316@gmail.com>
2025-04-14 17:41:26 -0300

    Add files via upload
    Exercitando conceitos básicos e orientação a objeto

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,56 @@
+public class Usuario {
+    public static void main(String[] args) {
+
+        SmartTV smartTV = new SmartTV();
+
+        System.out.println("--------------------------");
+        System.out.println("          STATUS          ");
+        System.out.println("Ligada: " + smartTV.ligada);
+        System.out.println("Canal: " + smartTV.canal);
+        System.out.println("Volume: " + smartTV.volume);
+        System.out.println("--------------------------");
+
+        System.out.println("       Atualização:       ");
+        smartTV.ligar();
+        smartTV.aumentarVolume();
+        smartTV.aumentarVolume();
+        smartTV.aumentarVolume();
+        smartTV.passarCanal();
+        smartTV.passarCanal();
+        System.out.println("--------------------------");
+        System.out.println("          STATUS          ");
+        System.out.println("Ligada: " + smartTV.ligada);
+        System.out.println("Canal: " + smartTV.canal);
+        System.out.println("Volume: " + smartTV.volume);
+        System.out.println("--------------------------");
+
+        System.out.println("       Atualização:       ");
+        smartTV.voltarCanal();
+        smartTV.diminuirVolume();
+        System.out.println("--------------------------");
+        System.out.println("          STATUS          ");
+        System.out.println("Ligada: " + smartTV.ligada);
+        System.out.println("Canal: " + smartTV.canal);
+        System.out.println("Volume: " + smartTV.volume);
+        System.out.println("--------------------------");
+
+        System.out.println("       Atualização:       ");
+        smartTV.definirCanal(9);
+        System.out.println("--------------------------");
+        System.out.println("          STATUS          ");
+        System.out.println("Ligada: " + smartTV.ligada);
+        System.out.println("Canal: " + smartTV.canal);
+        System.out.println("Volume: " + smartTV.volume);
+        System.out.println("--------------------------");
+
+        System.out.println("       Atualização:       ");
+        smartTV.desligar();
+        System.out.println("--------------------------");
+        System.out.println("          STATUS          ");
+        System.out.println("Ligada: " + smartTV.ligada);
+        System.out.println("Canal: " + smartTV.canal);
+        System.out.println("Volume: " + smartTV.volume);
+        System.out.println("--------------------------");
+
+    }
+}
