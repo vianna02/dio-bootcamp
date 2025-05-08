8f553aae8ad430cf8bdf5f70966d7ba1aa0228d3
vianna02 <vianalisboa316@gmail.com>
2025-05-07 21:24:46 -0300

    Exercícios - POO
    Esta pasta contém todos códigos dos exercícios de POO do bootcamp DIO.

---
+++ exercicios-poo/petshop/src/com/petshop/util/Requisito.java
@@ -0,0 +1,11 @@
+package com.petshop.util;
+
+public enum Requisito {
+    PET_PRESENTE,
+    PET_LIMPO,
+    LAVADORA_LIMPA,
+    DAR_BANHO,
+    LIMPAR_LAVADORA,
+    ABASTECER_AGUA,
+    ABASTECER_SHAMPOO
+}
